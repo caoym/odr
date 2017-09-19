5d7742d36a5b79ea49097273f5f9b1b6bf21ea6f
caoym <caoyangmin@gmail.com>
2017-09-19 22:18:51 +0800

    no message

--- __main__.py
+++ __main__.py
@@ -1,6 +1,12 @@
 # coding=utf-8
+
+import sys
+import getopt
+import numpy
 from odr import WordCluster, DocClassifier
 import cPickle as pickle
+from PIL import Image
+from words import get_features_from_image, adjust_slope, adjust_size, visualization_features
 
 def save(var, name):
     file = open(name, 'wb')
@@ -15,7 +21,8 @@ def load(name):
     return var
 
 
-def fit(input_dir, output_dir="/tmp/"):
+def fit(input_dir, output_dir="/Users/caoyangmin/Documents/"):
+
     """
     训练
     :param input_dir: 样板所在所在目录
@@ -26,7 +33,10 @@ def fit(input_dir, output_dir="/tmp/"):
     # 通过聚类训练出词汇分类器
     wc = WordCluster()
     wc.fit(input_dir)
-    save(wc, output_dir + "wc.dump")
+    #save(wc, output_dir + "wc.dump")
+    wc = load(output_dir + "wc.dump")
+    assert isinstance(wc, WordCluster)
+    #wc.fit(input_dir)
 
     # 通过标注的样品，训练分类器
     dc = DocClassifier(wc)
@@ -47,6 +57,22 @@ def predict(image_file, classifier_dump_file="/tmp/dc.dump"):
 
 if __name__ == '__main__':
 
-    print u"Usage: main fit samples_dir\nmain fit samples_dir"
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hfps:", ["help","fit","predict","sample="])
+    except getopt.error, msg:
+        print msg
+        print "for help use --help"
+        sys.exit(2)
+        # process options
+    for o, a in opts:
+        if o in ("-h", "--help"):
+            print __doc__
+            sys.exit(0)
+            # process arguments
 
-    fit()
+    #img = Image.open('/Users/caoyangmin/Documents/code/21.jpg').convert('L')
+    #img = numpy.array(img, 'uint8')
+    #features = get_features_from_image(img)
+    #i = 0
+
+    fit(u'/Users/caoyangmin/Documents/code/odr-samples')
